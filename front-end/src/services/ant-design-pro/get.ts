be7c3a1f5f028e0aa1154ba70bc9da325518cd20
Dennis <alicorn0618@outlook.com>
2023-01-11 19:30:51 +0800

    refactor: add some services

--- front-end/src/services/ant-design-pro/get.ts
+++ front-end/src/services/ant-design-pro/get.ts
@@ -1,30 +1,28 @@
 import { request } from 'umi';
 
-
 //获得点赞信息
-export async function getLikes(id?: number, token: string){
-  return request<API.Likes>('/getLikes/', {
-   method: 'GET',
-    headers: {'Content-Type': 'application/json'},
-    data: {id: id, token: token},
-  })
+export async function getLikes(id?: number, token: string) {
+  return request<API.Likes>('/api/getLikes/', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    data: { id: id, token: token },
+  });
 }
 
-
 //获得收藏信息
-export async function getStared(id?: number, token: string){
-  return request<API.Stared>('/getStared/', {
+export async function getStared(id?: number, token: string) {
+  return request<API.Stared>('/api/getStared/', {
     method: 'GET',
-    headers: {'Content-Type': 'application/json'},
-    data: {id: id, token: token},
-  })
+    headers: { 'Content-Type': 'application/json' },
+    data: { id: id, token: token },
+  });
 }
 
 //获得评论数
-export async function getComments(id?: number){
-  return request('/getStared/', {
+export async function getComments(id?: number) {
+  return request('/api/getStared/', {
     method: 'GET',
-    headers: {'Content-Type': 'application/json'},
-    data: {id: id},
-  })
+    headers: { 'Content-Type': 'application/json' },
+    data: { id: id },
+  });
 }
