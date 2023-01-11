3325853fc0fe48f02a4623dd3d4bc88fe9db72c5
Dennis <alicorn0618@outlook.com>
2023-01-11 23:01:02 +0800

    addsome services

--- front-end/src/services/ant-design-pro/getCourseProblems.ts
+++ front-end/src/services/ant-design-pro/getCourseProblems.ts
@@ -2,16 +2,21 @@
 /* eslint-disable */
 import { request } from 'umi';
 
+const getCourseProblems = async (courseID: number) => {
+  return request<number[]>(`/api/getCourseProblems/${courseID}/`);
+};
+
 /** 此处后端没有提供注释 GET /getCourseProblems/${param0}/ */
-export async function getCourseProblemsRead(
-  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
-  params: API.getCourseProblemsReadParams,
-  options?: { [key: string]: any },
-) {
-  const { id: param0, ...queryParams } = params;
-  return request<API.TProblemList>(`/getCourseProblems/${param0}/`, {
-    method: 'GET',
-    params: { ...queryParams },
-    ...(options || {}),
-  });
-}
+// export async function getCourseProblemsRead(
+//   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+//   params: API.getCourseProblemsReadParams,
+//   options?: { [key: string]: any },
+// ) {
+//   const { id: param0, ...queryParams } = params;
+//   return request<API.TProblemList>(`/getCourseProblems/${param0}/`, {
+//     method: 'GET',
+//     params: { ...queryParams },
+//     ...(options || {}),
+//   });
+// }
+export default getCourseProblems;
--- front-end/src/services/ant-design-pro/getUserCourses.ts
+++ front-end/src/services/ant-design-pro/getUserCourses.ts
@@ -3,16 +3,13 @@
 import { request } from 'umi';
 
 /** 此处后端没有提供注释 GET /getUserCourses/${param0}/ */
-export async function getUserCoursesRead(id: number | undefined) {
-  // const url = '/getUserCourses/' + String(id) + '/';
-  return request<API.UserCourseList>(`/getUserCourses/${id}`, {
+const getUserCourses = async (userID: number) => {
+  return request<number>(`/getUserCourses/${userID}/`, {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
   });
-}
+};
 
+export default getUserCourses;
 /** 此处后端没有提供注释 DELETE /getUserCourses/${param0}/ */
 // export async function getUserCoursesDelete(
 //   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
